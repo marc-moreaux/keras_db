937538e434d3bbda21da2e3eddfd56e94f22d5d2
Marc Moreaux - cuda <mr.moreaux@gmail.com>
2017-03-23 11:47:40 +0100

    Un-official split support

--- actions40.py
+++ actions40.py
@@ -12,8 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--data_path",
                     help="Number of train samples in the DB",
                     default="../actions40")
+parser.add_argument("--official",
+                    help="use the official splits (no test set) (true or false)",
+                    default="true")
+
 args = parser.parse_args()
 data_path = args.data_path
+official_splits = True if args.official == "true" else False
 if data_path[-1] != '/' : data_path += '/'
 
 
@@ -26,28 +31,45 @@ def build_dataset(official_splits = True):
                           not anotated set
     """
     from shutil import copyfile, rmtree
-    if os.path.isdir(data_path+'data'):
-        rmtree(data_path+'data')
-    os.mkdir(data_path+"data")
-    os.mkdir(data_path+"data/train")
-    os.mkdir(data_path+"data/valid")
+
+    dir_name = 'data' if official_splits == True else 'data2'
+
+    if os.path.isdir(data_path+dir_name):
+        rmtree(data_path+dir_name)
+    os.mkdir(data_path+dir_name)
+    os.mkdir(data_path+dir_name+"/train")
+    os.mkdir(data_path+dir_name+"/valid")
+    if official_splits == False:
+        os.mkdir(data_path+dir_name+"/test")
 
 
     # Get files with splits
+    # Get files with splits
     files = os.listdir(data_path+"ImageSplits/")
     for file_name in files:
-        if file_name != "actions.txt":
-            with open(data_path+"ImageSplits/"+file_name, 'r') as file:
-                for img_name in file:
+        if(file_name != "actions.txt" and
+           file_name != "test.txt" and
+           file_name != "train.txt"):
+            with open(data_path+"ImageSplits/"+file_name, 'r') as mfile:
+                for img_idx, img_name in enumerate(mfile.readlines()):
                     # Remove some bullshit on strings
                     img_name = img_name.strip('\n').strip('\t').strip('\r')
 
                     # eg. applauding_test.txt to test
                     set_name = file_name.split('_')[-1][:-4]
-                    if set_name == "test":
-                        set_name = "valid"
+                    if official_splits == True:
+                        # Rename test to valid
+                        if set_name == "test":
+                            set_name = "valid"
+                    else:
+                        # Create a split between valid and test
+                        if (set_name == "test") and (img_idx > 49):
+                            set_name = "valid"
 
-                    # eg. "looking_through_mic_047.jpg" to "looking_through_mic"
+                    if img_idx == 1000:
+                        print(img_idx, img_name, set_name)
+
+                    # eg. looking_through_mic_047.jpg to looking_through_mic
                     class_name = img_name.split("_")[:-1]
                     class_name = "_".join(class_name)
 
@@ -69,15 +91,15 @@ def build_dataset(official_splits = True):
                     pos_string = "_{}_{}_{}_{}".format(top,left,bottom,right)
                     img_name_new = img_name.split('.')[0]+pos_string+'.jpg'
 
-                    #
-                    if not os.path.isdir(data_path+"data/"+set_name+'/'+class_name):
-                        os.mkdir(data_path+"data/"+set_name+'/'+class_name+'/')
-                    copyfile(data_path+"images/"+img_name,
-                             data_path+"data/"+set_name+'/'+class_name+'/'+img_name_new)
+                    # mkdir if missing and copy file
+                    if not os.path.isdir(data_path+"data2/"+set_name+'/'+class_name):
+                        os.mkdir(data_path+"data2/"+set_name+'/'+class_name+'/')
+                    copyfile(data_path+"JPEGImages/"+img_name,
+                             data_path+"data2/"+set_name+'/'+class_name+'/'+img_name_new)
 
 
 
 
 
 
-build_dataset(official_splits=True)
+build_dataset(official_splits=official_splits)
